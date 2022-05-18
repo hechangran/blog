345afec26caaf790d2f1c0a2f1cc103a90a3b68e
Changran He <hechangran@gmail.com>
2022-05-18 12:15:35 +0800

    fix search

--- api/search-notion.ts
+++ api/search-notion.ts
@@ -10,9 +10,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   const searchParams: types.SearchParams = req.body
 
-  console.log('lambda search-notion', searchParams)
+  console.log('<<< lambda search-notion', searchParams)
   const results = await search(searchParams)
-
+  console.log('>>> lambda search-notion', results)
+  
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=60, max-age=60, stale-while-revalidate=60'
